22b3a327b92bd3d26eda3f5083bf8381a82b432b
dom <dom>
2002-11-27 15:12:24 +0000

    Python module to get the HTTP headers of a given resource (with a simple HEAD). Quasi-identical to original HTTP GET impl. in python

---
+++ http_head.py
@@ -0,0 +1,49 @@
+""" $Id$
+    Module to make a HTTP Head
+""" 
+
+from urllib import *
+
+class HEADURLopener(FancyURLopener):
+    def open_http(self, url):
+        import httplib
+        user_passwd = None
+        if type(url) is type(""):
+            host, selector = splithost(url)
+            if host:
+                user_passwd, host = splituser(host)
+                host = unquote(host)
+            realhost = host
+        else:
+            host, selector = url
+            urltype, rest = splittype(selector)
+            url = rest
+            user_passwd = None
+            if string.lower(urltype) != 'http':
+                realhost = None
+            else:
+                realhost, rest = splithost(rest)
+                if realhost:
+                    user_passwd, realhost = splituser(realhost)
+                if user_passwd:
+                    selector = "%s://%s%s" % (urltype, realhost, rest)
+            #print "proxy via http:", host, selector
+        if not host: raise IOError, ('http error', 'no host given')
+        if user_passwd:
+            import base64
+            auth = string.strip(base64.encodestring(user_passwd))
+        else:
+            auth = None
+        h = httplib.HTTP(host) 
+        h.putrequest('HEAD', selector)
+        if auth: h.putheader('Authorization', 'Basic %s' % auth)
+        if realhost: h.putheader('Host', realhost)
+        for args in self.addheaders: apply(h.putheader, args)
+        h.endheaders()
+        errcode, errmsg, headers = h.getreply()
+        fp = h.getfile()
+        if errcode == 200:
+            import urllib
+            return urllib.addinfourl(fp, headers, "http:" + url)
+        else:
+            return self.http_error(url, fp, errcode, errmsg, headers)
